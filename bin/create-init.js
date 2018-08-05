b2d5cb06a8cb2054f2eae635caeb27729c8e8a34
朱自伟 <2085188234@qq.comgit config --global user.email 2085188234@qq.com>
2018-08-05 11:09:03 +0800

    first commit

---
+++ bin/create-init.js
@@ -0,0 +1,64 @@
+program
+  .usage('<template-name> [project-name]');
+
+program.on('--help', function() {
+  console.log('  Examples:')
+  console.log()
+  console.log(chalk.yellow('    # 使用npm模板创建'))
+  console.log('    $ struct init template-name my-project')
+  console.log('    $ struct init webpack my-project')
+  console.log()
+});
+// init 命令的帮助文档
+function help() {
+  program.parse(process.argv)
+  if (program.args.length < 1) return program.help()
+}
+
+help();
+
+const template = program.args[0];
+const dir = program.args[1];
+const to = path.resolve(dir || '.');
+// 模板不存在将不执行初始化项目的操作
+if (!tplJson[template]) {
+  console.log(chalk.red(`template.json里没有${template}的模板信息，请添加！`));
+  return;
+}
+if (!dir || dir.indexOf('/') > -1) {
+  console.log(chalk.red('请输入项目名名称'));
+  return;
+}
+
+function run() {
+  console.log(chalk.yellow(`使用模板${template}创建项目`));
+  const spinner = ora('正在下载模板');
+  spinner.start();
+
+  // 下载模板到本地
+
+  exec(`tnpm i ${tplJson[template].npm}`, (err, data) => {
+    spinner.stop();
+    process.on('exit', () => {
+      rm(`${process.cwd()}/node_modules`)
+    })
+    if (err) {
+      console.log(chalk.red('模板下载失败 ', err.message));
+    }
+    const tplPath = `${process.cwd()}/node_modules/${tplJson[template].npm}`;
+    const opts = require(`${tplPath}/meta`);
+    const projectPath = `${process.cwd()}/${dir}`;
+
+    // 生成新项目
+
+    Metalsmith(`${tplPath}/template`)
+      .source('.')
+      .destination(`${projectPath}`)
+      .build(function(err) {
+        if (err) {
+          console.log(chalk.red('项目生成失败', err));
+        }
+        console.log(chalk.yellow(' \n 项目已创建'));
+      })
+  });
+}
---
+++ bin/create.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node    //必须放到文件开头
+
+const program = require('commander');
+
+program
+  .version(require('../package').version) //  显示版本号
+  .usage('<command> [options]');
+
+program
+  .command('list')
+  .description('显示所有模板')
+  .action(require('../lib/list'));
+
+program
+  .command('add')
+  .description('添加模板')
+  .action(require('../lib/add'));
+
+program
+  .command('delete')
+  //    .alias('d')
+  .description('删除模板')
+  .action(require('../lib/delete'));
